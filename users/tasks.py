19d4c3e8784da5d9bcd4b4c5200f667b4e51a6d2
andrey_mazo <andreymazo@mail.ru>
2023-03-29 15:55:19 +0300

    фоновые задачи выставлю в админке, работают при запуске метода гет после логирования при обновлении списка привычек

--- users/tasks.py
+++ users/tasks.py
@@ -28,45 +28,46 @@ CHAT_ID = settings.CHAT_ID
 def send_telegram():  #(self, request, *args, **kwargs)
     print('_______________________')
     textt = 'Time waits for nobody'
-    while True:
-        #if request.method == 'GET':
-            time.sleep(25)  ##Kazhdie 30 sec proveryaet pole time_to_doo_habit
+    #while True:
+    # if request.method == 'GET':
+            #time.sleep(25)  ##Kazhdie 30 sec proveryaet pole time_to_doo_habit
             # thread = threading.Thread(  # создание отдельного потока
             #     target=print, args=("данные сайта обновились",))
             # thread.start()
             # now = time.time()
-            now = datetime.datetime.now(pytz.timezone(settings.TIME_ZONE))
-            a = Habit.objects.all()  # time_to_doo_habit.second
-            for ii in User.objects.all():
-                for i in a.filter(client_id=ii):
-                    time_compare = (now - i.updated_at).total_seconds() % 86400
+    now = datetime.datetime.now(pytz.timezone(settings.TIME_ZONE))
+    a = Habit.objects.all()  # time_to_doo_habit.second
+    for ii in User.objects.all():
+        for i in a.filter(client_id=ii):
+            time_compare = (now - i.updated_at).total_seconds() % 86400
+            time.sleep(1)
+            try:
+                # print('(now - i.updated_at).total_seconds()-----', time_compare)##Ostatok secund v sutkah, ego potom sravnit nado
+                # print('i.time_to_doo_habit.second_____', i.time_to_doo_habit)
+                # i.time_to_doo_habit = datetime.now().time()
+                seconds = (
+                                  i.time_to_doo_habit.hour * 60 + i.time_to_doo_habit.minute) * 60 + i.time_to_doo_habit.second
+                # print(seconds)  ##Vsego secund vo vremeni dlia ispolnenia 86400 sec v sutk, ostatok ot 86400 nas interesuet
 
-                    try:
-                        # print('(now - i.updated_at).total_seconds()-----', time_compare)##Ostatok secund v sutkah, ego potom sravnit nado
-                        # print('i.time_to_doo_habit.second_____', i.time_to_doo_habit)
-                        # i.time_to_doo_habit = datetime.now().time()
-                        seconds = (
-                                              i.time_to_doo_habit.hour * 60 + i.time_to_doo_habit.minute) * 60 + i.time_to_doo_habit.second
-                        # print(seconds)  ##Vsego secund vo vremeni dlia ispolnenia 86400 sec v sutk, ostatok ot 86400 nas interesuet
+                if abs(seconds - time_compare) < 30:  # Esli menshe polminuti do vremeni, otsilaem
+                    print('Otsilaem *****************')  ###Zdes mozhno otsilat v telegramm
+                print('Ne otsilaem poka *****************')
 
-                        if abs(seconds - time_compare) < 30:  # Esli menshe polminuti do vremeni, otsilaem
-                            print('Otsilaem *****************')  ###Zdes mozhno otsilat v telegramm
-                        print('Ne otsilaem poka *****************')
+            except AttributeError as e:
+                print(e)
+                pass
+                # for i in a:
+                #     print(dir(i))
+                # delta = abs(seconds - now)
+        print('now-----77777777777-----', now)
+        ######################################################Commentim chtobi ne slalo ######################
+        # response=bot.sendMessage(CHAT_ID, textt)#501573655
+        # print(response)
+        ################################################################################3
+        # params = {'chat_id': '-524797982', 'text': 'OKEY!', 'reply_to_message_id': 1508}
+        # response = bot.getUpdates()
+        # now = datetime.datetime.now(pytz.timezone(settings.TIME_ZONE))
 
-                    except AttributeError as e:
-                        print(e)
-                        pass
-                        # for i in a:
-                        #     print(dir(i))
-                        # delta = abs(seconds - now)
-                print('now-----77777777777-----', now)
-                ######################################################Commentim chtobi ne slalo ######################
-                # response=bot.sendMessage(CHAT_ID, textt)#501573655
-                # print(response)
-                ################################################################################3
-                # params = {'chat_id': '-524797982', 'text': 'OKEY!', 'reply_to_message_id': 1508}
-                # response = bot.getUpdates()
-                # now = datetime.datetime.now(pytz.timezone(settings.TIME_ZONE))
 
         # return self.queryset(request, *args, **kwargs)
         #self.queryset
